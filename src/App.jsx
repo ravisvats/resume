823bf0c8ddb0bcc5d22163b77d198c9bed25d433
ravisvats <ravisvats@gmail.com>
2025-07-10 12:53:00 +0530

    sec commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Home from './component/homepage/'
 import Forms from "./component/forms/Forms";
